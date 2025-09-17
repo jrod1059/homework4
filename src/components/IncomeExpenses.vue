5e26c555ef78c7768dbefc1700ec3766d827a7e7
Jonathan Rodriguez <rodriguez.jonathan77@yahoo.com>
2025-09-17 18:07:00 -0500

    Finshed Income Expense Component.

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import {defineProps} from 'vue'
+    
+    const props = defineProps({
+        income: {
+            type: Number,
+            require: true,
+
+        },
+        expense: {
+            type: Number,
+            require: true,
+        }
+    })
+
+</script>
+
+<template>
+    <div class="inc-exp-container">
+        <div>
+            <h4>Income</h4>
+            <p id="Money-plus" class="money plus">+${{ income }}</p>
+        </div>
+
+        <div>
+            <h4>Expense</h4>
+            <p id="Money-minus" class="money minus">-${{ expense }}</p>
+        </div>
+    </div>
+
+
+</template>
